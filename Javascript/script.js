9d2d6063732ba5753b97d0959cc401b3046b545b
rlasko <rlasko@cmu.edu>
2016-11-29 18:37:53 -0500

    responsiveness restored

--- Javascript/script.js
+++
@@ -1,9 +0,0 @@
-// Pseudo code:
-//
-// Scrolling: - From affinity website
-//  create list of screen centers
-//   when user starts scroll, if crosses "sticky threshold":
-//     auto scroll to next point
-//
-
-// When click left side nav buttons, jump scroll to center of corresponding screen
