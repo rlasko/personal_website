f3b005ebbca5ace4e7de0f11e02e9a54cfa1d698
rlasko <rlasko@cmu.edu>
2016-11-29 14:43:49 -0500

    initial commit

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+// Pseudo code:
+//
+// Scrolling: - From affinity website
+//  create list of screen centers
+//   when user starts scroll, if crosses "sticky threshold":
+//     auto scroll to next point
+//
+
+// When click left side nav buttons, jump scroll to center of corresponding screen
